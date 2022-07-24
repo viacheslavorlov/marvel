1825db725c936401b5c83f65ad4043aabe7b952c
Viacheslav Orlov <viacheslavorlov@gmail.com>
2022-07-24 18:33:38 +0300

    CharList.js App.js CharList.js RandomChar.js переписаны на функциональные компоненты. загрузка по скроллу все еще не работает!!!

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,85 +1,72 @@
 import './randomChar.scss';
 // import thor from '../../resources/img/thor.jpeg';
 import mjolnir from '../../resources/img/mjolnir.png';
-import {Component} from "react";
+import {useEffect, useState} from "react";
 import MarvelService from "../../services/MarvelService";
 import {Spinner} from "../spinner/spinner";
 import {ErrorMessage} from "../ErrorMessage/ErrorMessage";
 
-class RandomChar extends Component {
-	constructor(props) {
-		super(props);
+const RandomChar = () => {
 
+	const [char, setChar] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(false);
+
+	const marvelService = new MarvelService();
+
+	const onCharloaded = (charfromserver) => {
+		setChar(charfromserver);
+		setLoading(false);
+		setError(false);
 	}
 
-	state = {
-		char: {},
-		loading: true,
-		error: false
+	const onError = () => {
+		setLoading(false);
+		setError(true);
 	}
 
-	marvelService = new MarvelService();
-
-	onCharloaded = (char) => {
-		this.setState({
-			char,
-			loading: false,
-			error: false
-		});
-	}
-
-	onError = () => {
-		this.setState({
-			loading: false,
-			error: true
-		});
-	}
-
-	updateCharacter = () => {
-		this.setState({loading: true});
+	const updateCharacter = () => {
+		setLoading(true);
 		const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-		this.marvelService
+		marvelService
 			.getCaracter(id)
-			.then(this.onCharloaded)
-			.catch(this.onError)
+			.then(onCharloaded)
+			.catch(onError)
 	}
 
-	componentDidMount() {
-		this.updateCharacter();/* ! нельзя вызывать в  конструкторе */
-	}
+	useEffect(() => {
+		updateCharacter();
+	}, []);
 
 
-	render() {
-		const {loading, char, error} = this.state;
-		const errorMessage = error ? <ErrorMessage/> : null;
-		const spinner = loading ? <Spinner/> : null;
-		const content = !(loading || error) ? <View char={char}/> : null;
+	const errorMessage = error ? <ErrorMessage/> : null;
+	const spinner = loading ? <Spinner/> : null;
+	const content = !(loading || error) ? <View char={char}/> : null;
 
-		return (
-			<div className="randomchar">
-				{errorMessage}
-				{spinner}
-				{content}
-				<div className="randomchar__static" onClick={this.updateCharacter}>
-					<p className="randomchar__title">
-						Random character for today!<br/>
-						Do you want to get to know him better?
-					</p>
-					<p className="randomchar__title">
-						Or choose another one
-					</p>
-					<button className="button button__main">
-						<div className="inner">try it</div>
-					</button>
-					<img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-				</div>
+	return (
+		<div className="randomchar">
+			{errorMessage}
+			{spinner}
+			{content}
+			<div className="randomchar__static" onClick={updateCharacter}>
+				<p className="randomchar__title">
+					Random character for today!<br/>
+					Do you want to get to know him better?
+				</p>
+				<p className="randomchar__title">
+					Or choose another one
+				</p>
+				<button className="button button__main">
+					<div className="inner">try it</div>
+				</button>
+				<img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
-const View = (props) => {
-	let {name, description, thumbnail, homepage, wiki} = props.char;
+const View = ({char}) => {
+	let {name, description, thumbnail, homepage, wiki} = char;
 	let fitObj;
 	if (description) {
 		if (description.length > 20) {
